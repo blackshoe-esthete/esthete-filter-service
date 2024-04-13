55fb06497fef789562f347d9da588903c1e599ab
Subin <70842920+bin-pro@users.noreply.github.com>
2024-04-13 15:42:16 +0900

    [EST-69]Add: exception template, response dto

---
+++ src/main/java/com/blackshoe/esthete/exception/FilterErrorResult.java
@@ -0,0 +1,16 @@
+package com.blackshoe.esthete.exception;
+
+
+import lombok.Getter;
+import lombok.RequiredArgsConstructor;
+import org.springframework.http.HttpStatus;
+
+@Getter
+@RequiredArgsConstructor
+public enum FilterErrorResult {
+    NOT_FOUND_FILTER(HttpStatus.NOT_FOUND, "존재하지 않는 필터입니다."),
+    ;
+
+    private final HttpStatus httpStatus;
+    private final String message;
+}
