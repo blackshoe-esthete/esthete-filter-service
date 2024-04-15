cfd62b5acd23e0e51808e478d7901445b2234041
Subin <70842920+bin-pro@users.noreply.github.com>
2024-04-15 14:08:29 +0900

    [EST-69]Refactor: extract userId with jwt service

--- src/main/java/com/blackshoe/esthete/controller/FilterController.java
+++ src/main/java/com/blackshoe/esthete/controller/FilterController.java
@@ -1,16 +1,14 @@
 package com.blackshoe.esthete.controller;
 
 import com.blackshoe.esthete.dto.FilterDto;
+import com.blackshoe.esthete.service.JwtService;
 import com.blackshoe.esthete.service.SearchService;
 import lombok.RequiredArgsConstructor;
 import lombok.extern.slf4j.Slf4j;
 import org.springframework.data.domain.Page;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
-import org.springframework.web.bind.annotation.GetMapping;
-import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.bind.annotation.RequestParam;
-import org.springframework.web.bind.annotation.RestController;
+import org.springframework.web.bind.annotation.*;
 
 import java.util.UUID;
 
@@ -21,14 +19,17 @@ import java.util.UUID;
 public class FilterController {
 
     private final SearchService searchService;
+    private final JwtService jwtService;
 
     @GetMapping("/searching")
     public ResponseEntity<Page<FilterDto.SearchFilterResponse>> searchFilter(
-            @RequestParam(name = "user_id") UUID userId,
+             @RequestHeader("Authorization") String accessToken,
              @RequestParam(required = false) String keyword,
              @RequestParam(required = false) UUID tagId,
              @RequestParam(required = false, defaultValue = "0") Integer page,
              @RequestParam(required = false, defaultValue = "10") Integer size) {
+
+        UUID userId = jwtService.extractUserId(accessToken);
 
         log.info("searchFilter userId: {}", userId.toString());
         log.info("searchFilter keyword: {}", keyword);
@@ -60,4 +61,17 @@ public class FilterController {
         return ResponseEntity.status(HttpStatus.OK).body(
                 searchService.searchAllByFilterNameOrWriterNameContainingAndHasTag(searchWithTagRequest, page, size));
     }
+
+    //제작 필터 리스트 조회
+
+    //구매 필터 리스트 조회
+
+    //필터 설정값 조회
+
+    //필터 썸네일, 대표사진 조회
+
+    //필터 상세보기
+
+    //필터 구매하기
+    @PostMapping
 }
