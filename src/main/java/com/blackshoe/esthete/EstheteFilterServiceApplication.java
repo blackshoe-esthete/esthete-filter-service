7c1d431bee988c663562b10a836b03ce52a3ab94
BoyoungH <hyong0903@naver.com>
2024-05-10 12:03:35 +0900

    [EST-65] Add: swagger server url 추가

--- src/main/java/com/blackshoe/esthete/EstheteFilterServiceApplication.java
+++ src/main/java/com/blackshoe/esthete/EstheteFilterServiceApplication.java
@@ -1,8 +1,13 @@
 package com.blackshoe.esthete;
 
+import io.swagger.v3.oas.annotations.OpenAPIDefinition;
+import io.swagger.v3.oas.annotations.servers.Server;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.data.jpa.repository.config.EnableJpaAuditing;
+
+
+@OpenAPIDefinition(servers = {@Server(url = "${swagger.server.url}", description = "Default Server url")})
 
 @SpringBootApplication
 @EnableJpaAuditing
