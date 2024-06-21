7edf721b6a7b293f53933f0b835890e97b810a30
Subin <70842920+bin-pro@users.noreply.github.com>
2024-06-21 15:04:51 +0900

    Chore: add dependencies for eureka server

--- src/main/java/com/blackshoe/esthete/EstheteFilterServiceApplication.java
+++ src/main/java/com/blackshoe/esthete/EstheteFilterServiceApplication.java
@@ -6,9 +6,16 @@ import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.data.jpa.repository.config.EnableJpaAuditing;
 
+import java.util.TimeZone;
+import jakarta.annotation.PostConstruct;
+
 @SpringBootApplication
 @EnableJpaAuditing
 public class EstheteFilterServiceApplication {
+	@PostConstruct
+	void started(){
+		TimeZone.setDefault(TimeZone.getTimeZone("Asia/Seoul"));
+	}
 
 	public static void main(String[] args) {
 		SpringApplication.run(EstheteFilterServiceApplication.class, args);
