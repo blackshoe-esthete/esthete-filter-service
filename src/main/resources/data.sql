b85632b03b95015af005ec6af9e9b442ba95a0e4
Subin <70842920+bin-pro@users.noreply.github.com>
2024-07-07 00:57:15 +0900

    Fix: tags_uuid -> tag_uuid

--- src/main/resources/data.sql
+++ src/main/resources/data.sql
@@ -26,7 +26,7 @@ INSERT INTO tags (tag_uuid, name) VALUES
                                        (UNHEX(REPLACE('b06da443-52c2-4398-9bdf-6a7f3f14f29f', '-', '')), '순수한'),
                                        (UNHEX(REPLACE('c5a5ff7b-0b40-4683-b796-5c295b1908a5', '-', '')), '고요한'),
                                        (UNHEX(REPLACE('ec2b0244-e37c-4fd0-8aee-c11c831124b3', '-', '')), '고급스러운')
-    ON DUPLICATE KEY UPDATE tags_uuid = tags_uuid;
+    ON DUPLICATE KEY UPDATE tag_uuid = tag_uuid;
 
 INSERT INTO users (user_uuid, nickname, created_at, updated_at, user_id, profile_img_url) VALUES
                                                                    (UNHEX(REPLACE('5a0db2eb-f4bc-4fa3-ae47-8381ed0da1ab', '-', '')), 'testuser', NOW(), NOW(), 1, 'default')
